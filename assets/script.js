1aa407f33e26022dbdc53451399d0b90d30754b9
BBT <emmagollan33@gmail.com>
2021-02-07 04:28:10 -0500

    created IDs for the food list items, started delete button

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@
 //   }
 
 
+var foodIdCounter = 0;
 var formEl = document.querySelector("#input-form");
 var inFridgeEl = document.querySelector("#in-fridge");
 // Adding to Fridge!
@@ -15,17 +16,55 @@ var addToFridge = function(event) {
 
     formEl.reset();
 
+    var foodDataObj = {
+        name: newFoodInput,
+    };
+
+    //this will make the item added to the list into a data object
+    createItemEl(foodDataObj);
+    
+};
+
+var createItemEl = function(foodDataObj) {
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "food-item";
-    listItemEl.textContent = newFoodInput;
-    console.log(newFoodInput);
-    inFridgeEl.appendChild(listItemEl);
+   
+    listItemEl.setAttribute("data-food-id", foodIdCounter);
 
-    
+    var foodInfoEl = document.createElement("div");
+    foodInfoEl.className = "food-info";
+    foodInfoEl.innerHTML = "<li class='food-item'>" + foodDataObj.name + "</li><span class='task-type'>" + "</span>";
+    listItemEl.appendChild(foodInfoEl);
+  
+    console.dir(listItemEl);
+
+    inFridgeEl.appendChild(listItemEl);
+  
+    // increase task counter for next unique id
+    foodIdCounter++;
+  };
+
+Var createDelete = function(foodId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+
+    // create delete button
+var deleteButtonEl = document.createElement("button");
+deleteButtonEl.textContent = "Delete";
+deleteButtonEl.className = "btn delete-btn";
+deleteButtonEl.setAttribute("data-food-id", foodId);
+
+actionContainerEl.appendChild(deleteButtonEl);
 
 
 
 };
 
+
+
 formEl.addEventListener("submit", addToFridge);
+
+
+
 
